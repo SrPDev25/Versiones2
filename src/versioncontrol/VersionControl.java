1d98091db472b28faa92f3b26191f7c63d9d08c0
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2022-01-13 09:41:27 +0100

    Primeros cambios

---
+++ src/versioncontrol/VersionControl.java
@@ -0,0 +1,19 @@
+
+package versioncontrol;
+
+/**
+ *
+ * @author dam
+ */
+public class VersionControl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola caracola");
+        
+    }
+    
+}
