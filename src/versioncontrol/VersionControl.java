a1e0b84930342442c9befb2082ea0c650d35dcd5
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2022-01-13 10:45:57 +0100

    Antes de subir a gitHub

--- src/versioncontrol/VersionControl.java
+++ src/versioncontrol/VersionControl.java
@@ -11,8 +11,8 @@ public class VersionControl {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hola caracola");
+        int unoMas1=7;
         
     }
     
